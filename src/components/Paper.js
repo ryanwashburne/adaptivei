f3473a75a0f3c34efb60569d1aa2bd988d4b5df6
ryanwashburne <ryan.washburne@gmail.com>
2019-09-04 09:07:04 -0700

    removed old

--- src/components/Paper.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import Paper from '@material-ui/core/Paper'
-import Box from './box'
-
-export default (props) => (
-  <Box clone {...props}><Paper>{props.children}</Paper></Box>
-)
--- src/components/Text.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import Typography from '@material-ui/core/Typography'
-
-export default (props) => (
-  <Typography {...props} style={{ fontFamily: 'inherit', lineHeight: 'inherit', ...props.style }} />
-)
